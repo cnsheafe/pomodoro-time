973c13c118424ec2800a820eb8c8827d4e82b563
cnsheafe <cnsheafe@gmail.com>
2017-05-11 18:38:13 -0500

    Animation now resets upon new inputs
    Layout reflow is triggered with offsetWidth in order to reset animations.
    JSS is responsible for stopwatch styling and animation.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,16 +3,44 @@ const jss = require('./jss');
 
 let clockDOM = document.querySelector('form.timer');
 
+const spinnerJSS = {
+  'clip': 'rect(auto 150px auto auto)',
+  'border-radius': '50%',
+  'z-index': '200',
+  'border': '2px solid green'
+};
+
+let fillerJSS = Object.assign({}, spinnerJSS);
+fillerJSS['z-index'] = '250';
+const maskJSS = {
+  'clip': 'rect(auto 150px auto auto)',
+  'z-index': '300',
+  'opacity': '1',
+  'background': 'white'
+};
+
 clockDOM.addEventListener('submit', function(event) {
   event.preventDefault();
   const timeLimit = document.getElementById('countdown').value*60;
   console.log(timeLimit);
+  // let
+  jss.remove();
+  let elm = document.getElementById('wrapper').firstElementChild;
+//elm.offsetWidth triggers layout reflow to reset the animation 
+  void elm.offsetWidth;
+
+  void elm.nextElementSibling.offsetWidth;
+  void elm.nextElementSibling.nextElementSibling.offsetWidth;
+
+  jss.set('.spinner',spinnerJSS);
+  jss.set('.filler', fillerJSS);
+  jss.set('.mask', maskJSS);
+
   const timerComponents = {
     '.spinner': `rota ${timeLimit}s linear 1 forwards`,
-    '.filler': `fill ${timeLimit/2}s steps(1,end) forwards`,
-    '.mask': `mask ${timeLimit/2}s steps(1,end) forwards`,
+    '.filler': `fill ${timeLimit/2}s steps(1,end) 1 forwards`,
+    '.mask': `mask ${timeLimit/2}s steps(1,end) 1 forwards`,
   };
-  let timerDom = document.getElementById('wrapper').firstElementChild;
   for (const cssClass in timerComponents) {
     jss.set(cssClass, {
       'animation': timerComponents[cssClass]
