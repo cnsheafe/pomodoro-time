a3972e594341baa279b090dd3a83571087737d7b
cnsheafe <cnsheafe@gmail.com>
2017-05-11 15:13:53 -0500

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,19 @@
+const jss = require('./jss');
+let clockDOM = document.querySelector('form.timer');
+clockDOM.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const timeLimit = document.getElementById('countdown').value*60;
+  console.log(timeLimit);
+  const timerComponents = {
+    '.spinner': `rota ${timeLimit}s linear 1 forwards`,
+    '.filler': `fill ${timeLimit/2}s steps(1,end) 1`,
+    '.mask-left': `mask-left ${timeLimit}s steps(1,end) 1`,
+    '.mask-right': 'mask-right ${timeLimit}s steps(1,end) 1'
+  };
+  let timerDom = document.getElementById('wrapper').firstElementChild;
+  for (const cssClass in timerComponents) {
+    jss.set(cssClass, {
+      'animation': timerComponents[cssClass]
+    });
+  }
+});
