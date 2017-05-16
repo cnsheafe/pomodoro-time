dc32eaab0db5aff94e5325b379709cda528e1044
cnsheafe <cnsheafe@gmail.com>
2017-05-16 14:23:04 -0500

    Now updates app state

--- public/scripts/event-listeners/modal.js
+++ public/scripts/event-listeners/modal.js
@@ -1,6 +1,13 @@
-function modalListener(modalDOM) {
+function modalListener(modalDOM, state) {
   modalDOM.addEventListener('click', event => {
     event.preventDefault();
-    if(event.target === )
+    if(event.target.classList.contains('mood-feedback')) {
+      state.feedback.mood = event.target.getAttribute('data-mood');
+    }
+    else if (event.target.getAttribute('type')==='submit') {
+      state.feedback.mood =  event.target.innerHTML;
+    }
   });
 }
+
+module.exports = {modalListener};
