1c3b367cdb6e48a0b194f899e1ad663c1179af2b
cnsheafe <cnsheafe@gmail.com>
2017-05-17 16:22:27 -0500

    Revised timer.
    Play button used for starting the timer.
    Background ring added for aesthetics and for
    UX cues. Timer setting moved below timer module.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
+const {countdownListener} = require('./event-listeners/countdown');
+const {modalListener} = require('./event-listeners/modal');
+
 let state = {
   feedback: {}
 };
-const {countdownListener} = require('./event-listeners/countdown');
-const {modalListener} = require('./event-listeners/modal');
+
 let clockDOM = document.getElementById('stopwatch');
 countdownListener(clockDOM);
 
 let modalsubmitDOM = document.getElementById('modal-submit');
 modalListener(modalsubmitDOM, state);
-
-// module.exports = {state};
