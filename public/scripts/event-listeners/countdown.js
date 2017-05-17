2cbbd67d37341b99c75ce80bfcf18f9e6332053a
cnsheafe <cnsheafe@gmail.com>
2017-05-17 16:21:36 -0500

    reflected changes in html

--- public/scripts/event-listeners/countdown.js
+++ public/scripts/event-listeners/countdown.js
@@ -4,22 +4,13 @@ const {renderTimer} = require('../jss/timer/timer');
 function countdownListener(clockDOM){
   clockDOM.addEventListener('submit', event => {
     event.preventDefault();
-    const timeLimit = document.getElementById('countdown').value*60;
-    const objectiveStatement = document.getElementById('objective-statement').value;
-    renderTimer(timeLimit, document.getElementById('wrapper'));
-
-    let stopwatchButton = document.getElementById('stopwatch-btn');
-    stopwatchButton.innerHTML = 'Stop';
-    stopwatchButton.removeAttribute('data-toggle');
-    stopwatchButton.removeAttribute('data-target');
-    stopwatchButton.removeAttribute('form');
+    const timeLimit = document.getElementById('countdown-work').value*60;
+    renderTimer(timeLimit, document.getElementById('stopwatch-display'));
 
     const timeout = window.setTimeout(function () {
       const alarm = new Audio('audio/alarm.mp3');
       alarm.play();
-      stopwatchButton.innerHTML = 'Start Break';
-      stopwatchButton.setAttribute('data-toggle', 'modal');
-      stopwatchButton.setAttribute('data-target', '#feedback-modal');
+      // stopwatchButton.innerHTML = 'Start Break';
     }, timeLimit*1000);
   });
 }
