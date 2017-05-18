f7c7d93b486dab7f8113cd7f4e9d2128f2028983
cnsheafe <cnsheafe@gmail.com>
2017-05-18 14:51:12 -0500

    Added cancel feature.
    Session can be ended early by hitting button a second time.
    Animations are canceled and so is timeout.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
 const {countdownListener} = require('./event-listeners/countdown');
 const {modalListener} = require('./event-listeners/modal');
-
+const jss = require('./jss/jss.min');
 let state = {
   feedback: {}
 };
 
 let clockDOM = document.getElementById('stopwatch');
-countdownListener(clockDOM);
+countdownListener(clockDOM, state, jss);
 
 let modalsubmitDOM = document.getElementById('modal-submit');
 modalListener(modalsubmitDOM, state);
