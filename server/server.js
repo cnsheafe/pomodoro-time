10d5941357b79a1c27312d7103d07b5b50ca3470
cnsheafe <cnsheafe@gmail.com>
2017-05-30 14:04:28 -0500

    Using routes

--- server/server.js
+++ server/server.js
@@ -2,93 +2,38 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const jsonParser = require('body-parser').json();
+const urlEncParser = require('body-parser').urlencoded({extended: true});
 const passport = require('passport');
 const {PORT} = require('./config');
 const {User} = require('./model');
 const strategy = require('./strategy');
-
-
+const routerSignup = require('./routes/signup');
+const routerLogin = require('./routes/login');
 const app = express();
 app.use(morgan('common'));
 app.use(express.static('public'));
-app.use(jsonParser);
-
-
-passport.use(strategy);
-app.use(passport.initialize());
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/data');
-
+app.use('/signup', routerSignup);
+app.use('/login', routerLogin);
 
 let server; //used if server is called for testing
-
-app.post('/signup', (req, res) => {
-	if (!req.body) {
-		return res.status(400).json({message: "Empty Request body"});
-	}
-	if (!('username' in req.body)) {
-		return res.status(422).json({
-			message: 'Missing field: username'
-		});
-	}
-
-	if (!('password' in req.body)) {
-		return res.status(422).json({
-			message: 'Missing field: password'
-		});
-	}
-	const credentials = req.body;
-	return User
-		.find({username: credentials.username})
-		.count()
-		.exec()
-		.then(count => {
-			if (count > 0) {
-				res.status(422).json({
-					message: 'username already taken'
-				});
-				throw "Username taken"
-			}
-			return User.hashPassword(credentials.password);
-		}, null)
-		.then(hash => {
-			return User.create({
-				username: credentials.username,
-				password: hash,
-				settings: {
-					work: 25,
-					break: 5
-				}
-			});
-		}, null)
-		.then(user => {
-			return res.status(201).json(user.apiRepr());
-		})
-		.catch(err => {
-			res.status(500).json({
-				msg: 'Internal server error'
-			})
-		});
-});
-
-app.post('/login', (req, res) => {
-	passport.authenticate('local',
-	data => {
-		if(data.valid) {
-			const user = data.user;
-			res.status(200).json({
-				username: user.username,
-				settings: user.settings,
-				history: user.history
-			});
-		}
-		else {
-			res.status(422).json({msg: data.msg})
-		}
-	})(req, res);
-	}
-);
+mongoose.connect('mongodb://localhost/data');
+// app.post('/login', (req, res) => {
+// 	passport.authenticate('local',
+// 	data => {
+// 		if(data.valid) {
+// 			const user = data.user;
+// 			res.status(200).json({
+// 				username: user.username,
+// 				settings: user.settings,
+// 				history: user.history
+// 			});
+// 		}
+// 		else {
+// 			res.status(422).json({msg: data.msg})
+// 		}
+// 	})(req, res);
+// 	}
+// );
 
 function runServer(port=PORT) {
 	return new Promise((resolve, reject) => {
