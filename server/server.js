71ca0f0cdb8d378b3bba889a58e134339be91b88
cnsheafe <cnsheafe@gmail.com>
2017-06-01 14:41:49 -0500

    Added route paths

--- server/server.js
+++ server/server.js
@@ -3,37 +3,31 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const jsonParser = require('body-parser').json();
 const urlEncParser = require('body-parser').urlencoded({extended: true});
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const {PORT} = require('./config');
 const {User} = require('./model');
 const strategy = require('./strategy');
 const routerSignup = require('./routes/signup');
 const routerLogin = require('./routes/login');
+const routerMe = require('./routes/me');
+const routerLogout = require('./routes/logout');
+
 const app = express();
 app.use(morgan('common'));
+app.use(jsonParser);
+app.use(urlEncParser);
+app.use(cookieParser());
+
 app.use(express.static('public'));
 app.use('/signup', routerSignup);
 app.use('/login', routerLogin);
+app.use('/me', routerMe);
+app.use('/logout', routerLogout);
 
 let server; //used if server is called for testing
 mongoose.connect('mongodb://localhost/data');
-// app.post('/login', (req, res) => {
-// 	passport.authenticate('local',
-// 	data => {
-// 		if(data.valid) {
-// 			const user = data.user;
-// 			res.status(200).json({
-// 				username: user.username,
-// 				settings: user.settings,
-// 				history: user.history
-// 			});
-// 		}
-// 		else {
-// 			res.status(422).json({msg: data.msg})
-// 		}
-// 	})(req, res);
-// 	}
-// );
+
 
 function runServer(port=PORT) {
 	return new Promise((resolve, reject) => {
