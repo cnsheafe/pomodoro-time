0106a2538b1edf8e3c1b3d68d4c16d2c031590b2
cnsheafe <cnsheafe@gmail.com>
2017-06-01 14:35:26 -0500

    Parse browser cookies to find relevant one for app

---
+++ public/scripts/general/cookie.js
@@ -0,0 +1,18 @@
+function parseCookieString(cookieString, state) {
+  console.log(cookieString);
+  if (typeof cookieString === 'string') {
+    if (cookieString.length > 0) {
+      cookieString.split(';').forEach(pair => {
+        let items = pair.split('=');
+        if(items[0] === 'pomodoro') {
+          state.cookie = {
+            name: items[0],
+            val: items[1]
+          };
+        }
+      });
+    }
+  }
+}
+
+module.exports = {parseCookieString};
