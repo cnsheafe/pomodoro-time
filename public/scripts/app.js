653a646d5944b5d34a247496e139ddfd3b4b9601
cnsheafe <cnsheafe@gmail.com>
2017-06-01 14:36:15 -0500

    Added ajax call to fetch user settings if authorized by cookie value

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,42 @@
 const {countdownListener} = require('./event-listeners/countdown');
 const {modalListener} = require('./event-listeners/modal');
 const jss = require('./jss/jss.min');
+const {parseCookieString} = require('./general/cookie');
 
 let state = {
+  username: null,
   feedback: {
     mood: null
   },
   settings: {
     work: null,
     break: null,
+  },
+  cookie: {
+    name: null,
+    val: null
   }
 };
+
+const cookies = document.cookie;
+state.username = window.location.search.slice(1);
+parseCookieString(cookies, state);
+$.ajax({
+  method: 'GET',
+  url: `http://localhost:8080/me`,
+  data: {
+    username: state.username,
+    id: state.cookie.val
+  }
+})
+.then(data => {
+  state.settings = data.settings;
+  state.history = data.history;
+  let buttonLink = document.querySelector('header > a');
+  buttonLink.classList.add('hide');
+  buttonLink.nextElementSibling.querySelector('button').textContent = 'Logout';
+});
+
 
 let clockDOM = document.getElementById('stopwatch');
 countdownListener(clockDOM, state, jss);
