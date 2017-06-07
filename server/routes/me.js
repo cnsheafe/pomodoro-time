8914b36c8e5182486f4c84aefaa72f2b967515a8
cnsheafe <cnsheafe@gmail.com>
2017-06-07 13:07:26 -0500

    Added endpoint for updating settings

--- server/routes/me.js
+++ server/routes/me.js
@@ -35,4 +35,20 @@ router.get('/', (req, res) => {
 	}
 });
 
+router.put('/', (req, res) => {
+  let toUpdate = {};
+  if  ('username' in req.body && 'id' in req.body) {
+    ['settings', 'history'].forEach(field => {
+      if (field in req.body) {
+        toUpdate[field] = req.body[field];
+      }
+    });
+  }
+  User
+    .findByIdAndUpdate(req.body.id, {$set: toUpdate})
+    .exec()
+    .then(post => res.status(204).json({msg: 'ok'}))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
 module.exports = router;
