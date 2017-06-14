c23b32db8d1d1d71a9a4fd98d51ab1423cc1c205
cnsheafe <cnsheafe@gmail.com>
2017-06-14 15:31:06 -0500

    Changed jss timer to timer in /render

--- public/scripts/event-listeners/countdown.js
+++ public/scripts/event-listeners/countdown.js
@@ -1,15 +1,18 @@
-const {renderTimer} = require('../jss/timer/timer');
-const reflow = require('../jss/reflow');
-const {togglePlayButton} = require('../render/toggle-play-button');
+const togglePlayButton = require('../render/toggle-play-button');
 
-function countdownListener(clockDOM, state, jss){
+function countdownListener(clockDOM, state) {
   clockDOM.addEventListener('submit', event => {
     event.preventDefault();
-    const wrapperElement = document.getElementById('stopwatch-display');
+    const wrapperElement = document.getElementById('timer-module');
+
     if(wrapperElement.querySelector('.glyphicon-play')) {
-      state.settings.work = document.getElementById('countdown-work').value*60;
-      state.settings.break = document.getElementById('countdown-break').value*60;
-      renderTimer(state.settings.work, document.getElementById('stopwatch-display'), jss);
+      state.settings.work = document
+        .getElementById('countdown-work').value*60;
+      state.settings.break = document
+        .getElementById('countdown-break').value*60;
+
+      state.timer.start(state.settigns.work*1e3);
+
       state.currentSession = {};
       state.currentSession.start = new Date();
       togglePlayButton(wrapperElement);
@@ -22,15 +25,15 @@ function countdownListener(clockDOM, state, jss){
 
       }, state.settings.work*1000);
       state.timeoutId = timeout;
-  }
+    }
 
   else if (wrapperElement.querySelector('.glyphicon-stop')) {
-    jss.remove();
     window.clearTimeout(state.timeoutId);
-    reflow.timer(wrapperElement.firstElementChild);
+    // reflow.timer(wrapperElement.firstElementChild);
+    state.timer.stop();
     togglePlayButton(wrapperElement);
     }
   });
 }
 
-module.exports = {countdownListener};
+module.exports = countdownListener;
