7c9473f5561e36137f9d1d96030df413490f7ccc
cnsheafe <cnsheafe@gmail.com>
2017-06-21 13:31:50 -0500

    Updated db path and added test account

--- server/config.js
+++ server/config.js
@@ -1,2 +1,5 @@
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/pomodoro";
+exports.DUMMY_USERNAME = process.env.DUMMY_USERNAME || "dummy";
+exports.DUMMY_PASS = process.env.DUMMY_PASS || "pass";
+// exports.ADMIN_KEY = process.env.ADMIN_KEY ||
