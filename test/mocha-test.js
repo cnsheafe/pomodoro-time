5f45bad1f4338c674f066259c1b9b540e06ba9a8
cnsheafe <cnsheafe@gmail.com>
2017-06-21 13:26:10 -0500

    Changed constants

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -9,41 +9,57 @@ const should = chai.should();
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-
 describe('API test framework', () => {
 
 	before(() => {return runServer(PORT);});
 	after(() => {return closeServer();});
 
 	describe('Homepage: /', () => {
-
 		it('should return index.html at root',() => {
-				return chai.request(app).get('/')
-					.then(res => {
-						res.should.have.status(200);
-					});
+			return chai.request(app).get('/')
+				.then(res => {
+					res.should.have.status(200);
 				});
- 			});
-	describe('User login and logout: /login, /me, /logout', function() {
-		let agent = chai.request.agent(app);
+		});
+ 	});
 
-		describe('Cookie sending and redirecting', function() {
+	describe('User account: /signup, /login, /me, /logout', function() {
+		let agent = chai.request.agent(app);
+		after(() => {
+			return User
+				.findOneAndRemove({username: DUMMY_USERNAME})
+				.exec();
+		});
+
+		describe('User account creation: /signup', function() {
+			it('should create an account', function() {
+				return agent
+					.post('/signup')
+					.send({username: DUMMY_USERNAME, password: DUMMY_PASS})
+					.then(res => {
+						res.should.redirect;
+						res.should.have.status(200);
+					})
+			})
+		});
+
+		describe('User login: /login', function() {
 			it('should redirect and send a cookie', function() {
 				return agent
 					.post('/login')
-					.send({username: 'demo', password: 'password'})
+					.send({username: DUMMY_USERNAME, password: DUMMY_PASS})
 					.then(res => {
 						expect(res).to.redirect;
 						res.should.have.status(200);
 					});
 			})
-		})
+		});
 
-		describe('User data endpoint', function() {
+		describe('User data: /me ', function() {
 			it('should be able to get user data', function() {
 				return agent
 					.get('/me')
-					.query({'username': 'demo'})
+					.query({'username': DUMMY_USERNAME})
 					.then(res => {
 						res.should.have.status(200);
 						const body = res.body;
@@ -65,29 +81,4 @@ describe('API test framework', () => {
 			});
 		});
 	});
-
-	describe('User account creation: /signup', function() {
-		it('should create an account', function() {
-			return chai.request(app)
-				.post('/signup')
-				.send({username: DUMMY_USERNAME, password: DUMMY_PASS})
-				.then(res => {
-					res.should.redirect;
-					res.should.have.status(200);
-				})
-				.then(() => {
-					User.findOneAndRemove({username: DUMMY_USERNAME})
-					.exec()
-				})
-				.then(() => {
-					User.findOne({username: DUMMY_USERNAME})
-					.exec()
-				})
-				.then(user => {
-					if(user) {
-						throw 'Unsuccessful removal'
-					}
-				})
-		})
-	})
 });
