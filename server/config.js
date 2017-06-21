67da7d720c04da2dfd797522c60a561fec692101
cnsheafe <cnsheafe@gmail.com>
2017-06-21 15:09:26 -0500

    Sanity check on PORT for Heroku

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
-exports.PORT = process.env.PORT || 8080;
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/pomodoro";
-exports.DUMMY_USERNAME = process.env.DUMMY_USERNAME || "dummy";
-exports.DUMMY_PASS = process.env.DUMMY_PASS || "pass";
+PORT = process.env.PORT || 8080;
+DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/pomodoro";
+DUMMY_USERNAME = process.env.DUMMY_USERNAME || "dummy";
+DUMMY_PASS = process.env.DUMMY_PASS || "pass";
 // exports.ADMIN_KEY = process.env.ADMIN_KEY ||
+module.exports = {PORT, DATABASE_URL, DUMMY_USERNAME, DUMMY_PASS};
