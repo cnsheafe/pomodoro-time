b77c4d69b06dcbcd86d9125a6ce3eba3dea7d667
cnsheafe <cnsheafe@gmail.com>
2017-06-21 20:09:02 -0500

    No longer need jss

--- public/scripts/jss/reflow.js
+++
@@ -1,9 +0,0 @@
-const reflow = {
-  timer: function(elm) {
-  void elm.offsetWidth;
-  void elm.nextElementSibling.offsetWidth;
-  void elm.nextElementSibling.nextElementSibling.offsetWidth;
-  }
-};
-
-module.exports = reflow;
