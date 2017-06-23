774283b05340d1043b66507f55ecdaad192fe8cf
cnsheafe <cnsheafe@gmail.com>
2017-06-23 13:17:14 -0500

    element.value retrieves input value; not getAttribute

--- public/scripts/event-listeners/settings.js
+++ public/scripts/event-listeners/settings.js
@@ -2,15 +2,23 @@ function settingsListener(settings, state) {
   settings.addEventListener('click', event => {
     event.target.textContent = 'Saving...';
     event.target.setAttribute('disabled','disabled');
+
+    state.settings.work = document.getElementById('default-work').value;
+    state.settings.break = document.getElementById('default-break').value;
+    console.log(`Work ${state.settings.work}`);
+    console.log(`Break ${state.settings.break}`);
+    document.getElementById('countdown-work').setAttribute('value', state.settings.work);
+    document.getElementById('countdown-break').setAttribute('value', state.settings.break);
+
     $.ajax({
       method: 'PUT',
       url: 'http://localhost:8080/me',
       data : {
-        id: state.id,
+        id: state.cookie.val,
         username: state.username,
         settings: {
-          work: document.getElementById('default-work').getAttribute('value'),
-          break: document.getElementById('default-break').getAttribute('value'),
+          work: state.settings.work,
+          break: state.settings.break,
         }
       }
     })
