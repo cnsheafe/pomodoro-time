b7333e003102efb59152bea4bbc0bb7333a89ead
cnsheafe <cnsheafe@gmail.com>
2017-06-24 13:55:45 -0500

    Added helper function to apply chart changes

--- public/scripts/event-listeners/timeline.js
+++ public/scripts/event-listeners/timeline.js
@@ -2,16 +2,23 @@ const {drawTimeline, populateTable} = require('../render/table');
 
 
 function timelineListener(timeline, state) {
+
   timeline.addEventListener('click', event => {
     event.preventDefault();
-    google.charts
-      .load('current', {'packages':['timeline']});
-    const data = populateTable(state);
-    google.charts.setOnLoadCallback(
-      function() {
-        drawTimeline(data);
-    });
+    timelineHelper(state, document.getElementById('timeline-page'));
   });
+  window.onresize = function() {
+    timelineHelper(state, document.getElementById('timeline-page'));
+  };
 }
 
+function timelineHelper(state, width) {
+  google.charts
+    .load('current', {'packages':['timeline']});
+  const data = populateTable(state);
+  google.charts.setOnLoadCallback(
+    function() {
+      drawTimeline(data, width);
+  });
+}
 module.exports = timelineListener;
